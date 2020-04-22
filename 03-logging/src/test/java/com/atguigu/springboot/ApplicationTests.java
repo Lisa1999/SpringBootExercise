38527dab5ec5455ad128e31f2a1a528d61b5d56f
Lisa1999 <291098574@qq.com>
2020-04-22 22:38:44 +0800

    springboot第2次提交

--- 03-logging/src/test/java/com/atguigu/springboot/ApplicationTests.java
+++ 03-logging/src/test/java/com/atguigu/springboot/ApplicationTests.java
@@ -1,13 +1,32 @@
 package com.atguigu.springboot;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class ApplicationTests {
+public class ApplicationTests {
 
+    //记录器
+   Logger logger = LoggerFactory.getLogger(getClass());
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+       //日志级别trace<debug<info<warn<error
+        //调整日志输出级别，日志在这个级别以后的高级别生效
+        logger.trace("trace日志。。。");
+        logger.debug("debug日志。。。");
+        //SB 默认调整info
+        logger.info("info日志。。。");
+        logger.warn("warn日志。。。");
+        logger.error("error日志。。。");
+
+
     }
 
 }
